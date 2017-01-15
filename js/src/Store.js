55e91ecec72df97e8d6764ad662cdf74673d04c5
Noor ul haq <noor_ul_haq96@hotmail.com>
2017-01-16 04:07:39 +0500

    final touche to gh-pages

--- js/src/Store.js
+++
@@ -1,8 +0,0 @@
-import {createStore, applyMiddleware} from "redux";
-import thunk from "redux-thunk";
-import createLogger from "redux-logger";
-import mainReducer from "./Reducers";
-
-let middleware = applyMiddleware(thunk,createLogger());
-
-export default createStore(mainReducer,middleware);
